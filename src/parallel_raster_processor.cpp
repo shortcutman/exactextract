9a96d4c986d0da8ff38aab96b3c2836b0ae84e18
Daniel Burke <danieljburke@me.com>
2023-11-25 15:10:22 +1100

    Half done raster parallel processor

--- src/parallel_raster_processor.cpp
+++ src/parallel_raster_processor.cpp
@@ -2,13 +2,13 @@
 
 #include <stdexcept>
 
+#include "feature_source.h"
 #include "grid.h"
 #include "operation.h"
 #include "parallel_raster_processor.h"
 #include "processor.h"
 #include "stats_registry.h"
 #include "operation.h"
-#include "raster_sequential_processor.h"
 #include "raster_source.h"
 
 #include <map>
@@ -16,6 +16,7 @@
 #include <set>
 
 #include <oneapi/tbb.h>
+#include <unordered_map>
 
 namespace {
 
@@ -37,91 +38,100 @@ void errorHandlerParallel(const char *fmt, ...) {
 namespace exactextract {
 
     struct RasterBlock {
-        Operation* _op;
         Grid<bounded_extent> _grid;
-        std::unique_ptr<AbstractRaster<double>> _raster;
+        std::set<std::pair<RasterSource*, RasterSource*>> processed;
+        std::map<RasterSource*, std::unique_ptr<AbstractRaster<double>>> raster_values;
+        StatsRegistry _reg;
+        std::vector<const Feature *> _hits;
 
         RasterBlock() : _grid(Grid<bounded_extent>::make_empty()) {}
 
-        RasterBlock(Operation* o, Grid<bounded_extent> g, std::unique_ptr<AbstractRaster<double>> r)
-        : _op(o), _grid(g), _raster(std::move(r)) {}
+        RasterBlock(Grid<bounded_extent> g)
+        : _grid(g) {}
     };
-
-    typedef std::vector<std::pair<std::string, geom_ptr_r>> GeometryBatch;
-    typedef std::shared_ptr<GeometryBatch> GeometryBatchPtr;
-    typedef std::pair<GeometryBatchPtr, std::shared_ptr<StatsRegistry>> BatchAndResults;
-    const int BatchSize = 1000;
 
     void ParallelRasterProcessor::read_features() {
         while (m_shp.next()) {
-            Feature feature = std::make_pair(
-                    m_shp.feature_field(m_shp.id_field()),
-                    geos_ptr(m_geos_context, m_shp.feature_geometry(m_geos_context)));
-            m_features.push_back(std::move(feature));
+            const Feature& feature = m_shp.feature();
+            MapFeature mf(feature);
+            m_features.push_back(std::move(mf));
         }
     }
 
     void ParallelRasterProcessor::populate_index() {
-        for (const Feature& f : m_features) {
+        for (const auto& f : m_features) {
             // TODO compute envelope of dataset, and crop raster by that extent before processing?
-            GEOSSTRtree_insert_r(m_geos_context, m_feature_tree.get(), f.second.get(), (void *) &f);
+            GEOSSTRtree_insert_r(m_geos_context, m_feature_tree.get(), f.geometry(), (void *) &f);
         }
     }
 
     void ParallelRasterProcessor::process() {
+        read_features();
+        populate_index();
 
         for (const auto& op : m_operations) {
             m_output.add_operation(*op);
         }
-        bool store_values = StatsRegistry::requires_stored_values(m_operations);
 
-        read_features();
-        populate_index();
-        auto grid = common_grid(m_operations.begin(), m_operations.end());
-        auto subdividedGrid = subdivide(grid, m_max_cells_in_memory);
+        bool store_values = StatsRegistry::requires_stored_values(m_operations);
+        auto operationsGridExtent = common_grid(m_operations.begin(), m_operations.end());
+        auto subdividedGrid = subdivide(operationsGridExtent, 10000);
 
         oneapi::tbb::enumerable_thread_specific<GEOSContextHandle_t> geos_context([] () -> GEOSContextHandle_t {
             return initGEOS_r(errorHandlerParallel, errorHandlerParallel);
         });
 
         oneapi::tbb::parallel_pipeline(6,
-            oneapi::tbb::make_filter<void, RasterBlock>(oneapi::tbb::filter_mode::serial_in_order,
+            oneapi::tbb::make_filter<void, std::shared_ptr<RasterBlock>>(oneapi::tbb::filter_mode::serial_in_order,
             //read features and setup batch for processing
-            [&grid, &subdividedGrid, &geos_context, this] (oneapi::tbb::flow_control& fc) -> RasterBlock {
-                //m_shp should only be accessed here
-                //iterate through m_shp
-                //return name + geom
-
+            [&operationsGridExtent, &subdividedGrid, &geos_context, this] (oneapi::tbb::flow_control& fc) -> std::shared_ptr<RasterBlock> {
+                //iterate through subdivisions and do file IO
                 if (subdividedGrid.begin() == subdividedGrid.end()) {
                     fc.stop();
-                    return RasterBlock();
+                    return std::shared_ptr<RasterBlock>();
                 }
 
-                auto op = m_operations.begin()->get();
                 auto subgrid = *subdividedGrid.begin();
 
-                RasterBlock block{
-                    op,
-                    subgrid,
-                    op->values->read_box(subgrid.extent().intersection(op->values->grid().extent()))
-                };
+                auto rasterBlock = std::make_shared<RasterBlock>(subgrid);
+
+                std::set<std::pair<RasterSource*, RasterSource*>> processed;
+                for (const auto &op : m_operations) {
+                    auto key = std::make_pair(op->weights, op->values);
+                    if (processed.find(key) != processed.end()) {
+                        continue;
+                    } else {
+                        processed.insert(key);
+                    }
+
+                    if (!op->values->grid().extent().contains(subgrid.extent())) {
+                        continue;
+                    }
+
+                    if (op->weighted() && !op->weights->grid().extent().contains(subgrid.extent())) {
+                        continue;
+                    }
+
+                    auto values = rasterBlock->raster_values[op->values].get();
+                    if (values == nullptr) {
+                        rasterBlock->raster_values[op->values] = op->values->read_box(subgrid.extent().intersection(op->values->grid().extent()));
+                        values = rasterBlock->raster_values[op->values].get();
+                    }
+                }                
 
                 subdividedGrid.erase(subdividedGrid.begin());
-                return block;
+                return rasterBlock;
             }) &
             //process batch
-            oneapi::tbb::make_filter<RasterBlock, StatsRegistry>(oneapi::tbb::filter_mode::parallel,
-            [&geos_context, &featureTree = m_feature_tree] (const RasterBlock& block) -> StatsRegistry {
+            oneapi::tbb::make_filter<std::shared_ptr<RasterBlock>, std::shared_ptr<RasterBlock>>(oneapi::tbb::filter_mode::parallel,
+            [&geos_context, &featureTree = m_feature_tree, this] (std::shared_ptr<RasterBlock> block) -> std::shared_ptr<RasterBlock> {
                 //intersect with raster
                 //return m_reg + name
                 auto& threadGeosContext = geos_context.local();
                 // bool store_values = StatsRegistry::requires_stored_values(ops);
                 bool store_values = true;
-                StatsRegistry reg;
 
-                auto query_rect = geos_make_box_polygon(threadGeosContext, block._grid.extent());
-                std::vector<const Feature *> hits;
-                auto values = block._raster.get();
+                auto query_rect = geos_make_box_polygon(threadGeosContext, block->_grid.extent());
 
                 GEOSSTRtree_query_r(threadGeosContext,
                     featureTree.get(),
@@ -131,30 +141,45 @@ namespace exactextract {
                         auto vec = static_cast<std::vector<const Feature *> *>(userdata);
 
                         vec->push_back(feature);
-                    }, &hits);
+                    }, &block->_hits);
                 
-                for (const auto& f : hits) {
-                    std::unique_ptr<Raster<float>> coverage = std::make_unique<Raster<float>>(
-                        raster_cell_intersection(block._grid, threadGeosContext, f->second.get()));
+                for (const auto& f : block->_hits) {
+                    std::string fid = f->get_string(m_shp.id_field());
+                    std::unique_ptr<Raster<float>> coverage = std::make_unique<Raster<float>>(raster_cell_intersection(block->_grid, threadGeosContext, f->geometry()));
                     
-                    
-                    reg.stats(f->first, *block._op, store_values).process(*coverage, *values);
+                    for (const auto &op : m_operations) {
+                        auto values = block->raster_values[op->values].get();
+
+                        if (op->weighted()) {
+                            auto weights = block->raster_values[op->weights].get();
+                            block->_reg.stats(fid, *op, store_values).process(*coverage, *values, *weights);
+                        } else {
+                            block->_reg.stats(fid, *op, store_values).process(*coverage, *values);
+                        }
+                    }
                 }
 
-                return reg;
+                return block;
             }) &
             //write out batch results
-            oneapi::tbb::make_filter<StatsRegistry, void>(oneapi::tbb::filter_mode::serial_out_of_order,
-            [&output = m_output] (const StatsRegistry& reg) {
-                //output
-                //m_output should only be accessed here
-                output.set_registry(&reg);
+            oneapi::tbb::make_filter<std::shared_ptr<RasterBlock>, void>(oneapi::tbb::filter_mode::serial_out_of_order,
+            [this] (std::shared_ptr<RasterBlock> block) {
 
-                for (auto& feature : reg.m_feature_stats) {
-                    output.write(feature.first);
+                for (const auto& f_in : block->_hits) {
+                    auto f_out = m_output.create_feature();
+                    std::string fid = f_in->get_string(m_shp.id_field());
+                    f_out->set(m_shp.id_field(), fid);
+                    for (const auto& col: m_include_cols) {
+                        f_out->set(col, *f_in);
+                    }
+                    for (const auto& op : m_operations) {
+                        op->set_result(m_reg, fid, *f_out);
+                    }
+                    m_output.write(*f_out);
+                    m_reg.flush_feature(fid);
                 }
 
-                std::cout << "Wrote " << reg.m_feature_stats.size() << " features." << std::endl;
+                std::cout << "Wrote " << block->_hits.size() << " features." << std::endl;
             }));
     }
 
--- src/parallel_raster_processor.h
+++ src/parallel_raster_processor.h
@@ -2,14 +2,14 @@
 #define EXACTEXTRACT_PARALLEL_RASTER_PROCESSOR_H
 
 #include "geos_utils.h"
+#include "map_feature.h"
 #include "processor.h"
 
 namespace exactextract {
 
     class ParallelRasterProcessor : public Processor {
     private:
-        using Feature=std::pair<std::string, geom_ptr_r>;
-        std::vector<Feature> m_features;
+        std::vector<MapFeature> m_features;
         tree_ptr_r m_feature_tree{geos_ptr(m_geos_context, GEOSSTRtree_create_r(m_geos_context, 10))};
 
     public:
